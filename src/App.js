b51454b921137b116c12c982572e9a224f872952
Keone Martin <keonemartin@gmail.com>
2021-09-02 18:44:26 +1000

    added a custom button and a userslist component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import AddUser from './components/Users/AddUser';
+import UsersList from './components/Users/UsersList';
 
+import { useState } from 'react';
 function App() {
+  const [users, setUsers] = useState([]);
+
   return (
     <div>
       <AddUser />
+      <UsersList users={users} />
     </div>
   );
 }
